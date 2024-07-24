0a2b8b39cae8dc78e3cc32f95ca0b55fa70963fb
unknown <jschott128@gmail.com>
2024-07-24 11:12:57 -0400

    created and added base template

---
+++ main.py
@@ -0,0 +1,32 @@
+#Password encoder and decoder
+#each instance of x=1 is a place holder
+
+#encoder
+def encode(password):
+    #Jason Schott's function, adds 3 to each digit
+    x=1
+
+#decoder
+def decode(password):
+    #Lance's function, Subtracts 3 from each digit
+    x=1
+
+#menu
+def printmenu():
+    """Menu
+    -------------
+    1. Encode
+    2. Decode
+    3. Quit
+
+    Please enter an option:
+    """
+    x=1    
+
+#main
+def main():
+    #runs print menu then either calls encode or decode, or quits
+    x=1
+
+if __name__ == "__main__":
+    main()
